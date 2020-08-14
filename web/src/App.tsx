2894e40b338f28e7cf2a656d4911c33988f2de2d
JoaoRibeiro2 <joaoribeiro2lokao@gmail.com>
2020-08-14 11:49:55 -0300

    feat: create application routes

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Routes from './routes';
+
+
+import "./assets/styles/global.css";
+
+//JSX - JavaScirpt + XML
+
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
