671da5ec7e8f874d80a52390bc33d95ca4bbdd81
João Victor <jv782063@gmail.com>
2020-08-14 19:30:39 -0300

    server(knex): creates knex configuration file

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -1,12 +1,46 @@
-import path from 'path'
+import path from 'path';
 
 module.exports = {
-	client: 'sqlite3',
-	connection: {
-	filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
-	},
-	migrations: {
-		directory: path.resolve(__dirname, 'src', 'database', 'migrations')
-	},
-	useNullAsDefault: true,
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
+    },
+    migrations: {
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+    },
+    useNullAsDefault: true,
+  },
+
+  // staging: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password"
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations"
+  //   }
+  // },
+
+  // production: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password"
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations"
+  //   }
+  // }
 };
